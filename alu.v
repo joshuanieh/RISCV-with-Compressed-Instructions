0562b400b87d2c3a54863dba90120e5568886547
joshuanieh <b07502027@ntu.edu.tw>
2023-05-26 13:04:04 +0800

    need debug

--- alu.v
+++ alu.v
@@ -3,11 +3,16 @@ Module: alu
 Author: Chia-Jen Nieh
 Description:
     Reading ALUCtrl_i and output corresponding results. 
-    When ALUCtrl_i == 3'd0. it handles ADD, LW, SW and JALR.
-    When ALUCtrl_i == 3'd1. it handles SUB and BEQ.
-    When ALUCtrl_i == 3'd2. it handles AND.
-    When ALUCtrl_i == 3'd3. it handles OR.
-    When ALUCtrl_i == 3'd4. it handles SLT.
+    When ALUCrtl_i == 4'd0. it handles ADD, ADDI, LW, SW and JALR.
+    When ALUCrtl_i == 4'd1. it handles SUB.
+    When ALUCrtl_i == 4'd2. it handles AND and ANDI.
+    When ALUCrtl_i == 4'd3. it handles OR and ORI.
+    When ALUCrtl_i == 4'd4. it handles SLT and SLTI.
+    When ALUCrtl_i == 4'd5. it handles XOR, XORI, BEQ.
+    When ALUCrtl_i == 4'd6. it handles SLLI.
+    When ALUCrtl_i == 4'd7. it handles SRLI.
+    When ALUCrtl_i == 4'd8. it handles SRAI.
+    When ALUCrtl_i == 4'd9. it handles BNE.
 */
 module alu (
     ALUCtrl_i,
@@ -18,38 +23,63 @@ module alu (
 );
     //ISA to be handled
     //ADD:  0000000      rs2 rs1 000 rd          0110011
+    //ADDI: IMM[11:0]        rs1 000 rd          0010011
     //LW:   IMM[11:0]        rs1 010 rd          0000011
     //SW:   IMM[11:5]    rs2 rs1 010 IMM[4:0]    0100011
     //JALR: IMM[11:0]        rs1 000 rd          1100111 //Byte address
 
     //SUB:  0100000      rs2 rs1 000 rd          0110011
-    //BEQ:  IMM[12,10:5] rs2 rs1 000 IMM[4:1,11] 1100011 //Halfword address //Partially handled by ALU
 
     //AND:  0000000      rs2 rs1 111 rd          0110011
+    //ANDI: IMM[11:0]        rs1 111 rd          0010011
 
     //OR:   0000000      rs2 rs1 110 rd          0110011
+    //ORI:  IMM[11:0]        rs1 110 rd          0010011
 
-    //SLT:  0000000      rs2 rs1 010 rd          0110011
+    //XOR:  0000000      rs2 rs1 100 rd          0110011
+    //XORI: IMM[11:0]        rs1 100 rd          0010011
+    //BEQ:  IMM[12,10:5] rs2 rs1 000 IMM[4:1,11] 1100011 //Halfword address //Partially handled by ALU, PC + imm is outside of ALU
     
-    //JAL:  IMM[20,10:1,11,19:12]    rd          1101111 //Halfword address //Not handled by ALU
-    `define ADD 3'd0
-    `define SUB 3'd1
-    `define AND 3'd2
-    `define OR  3'd3
-    `define SLT 3'd4
+    //BNE:  IMM[12,10:5] rs2 rs1 001 IMM[4:1,11] 1100011 //Halfword address //Partially handled by ALU, PC + imm is outside of ALU
+    
+    //SLLI: 0000000      sha rs1 001 rd          0010011
+
+    //SRLI: 0000000      sha rs1 101 rd          0010011
+
+    //SRAI: 0100000      sha rs1 101 rd          0010011
+    
+    //SLT:  0000000      rs2 rs1 010 rd          0110011
+    //SLTI: IMM[11:0]        rs1 010 rd          0010011
+    
+    //JAL:  IMM[20,10:1,11,19:12]    rd          1101111 //Halfword address //Not handled by ALU, PC + imm is outside of ALU
+
+    `define ADD 4'd0
+    `define SUB 4'd1
+    `define AND 4'd2
+    `define OR  4'd3
+    `define SLT 4'd4
+    `define XOR 4'd5
+    `define SLL 4'd6
+    `define SRL 4'd7
+    `define SRA 4'd8
+    `define BNE 4'd9
 
     input      [31:0] data1_i, data2_i;
-    input      [2:0]  ALUCtrl_i;
+    input      [3:0]  ALUCtrl_i;
     output reg        zero_o;
     output reg [31:0] data_o;
 
-    reg [31:0] add_result, sub_result, and_result, or_result;
+    reg [31:0] add_result, sub_result, and_result, or_result, xor_result, sll_result, srl_result, sra_result;
 
     always @(*) begin
         add_result = data1_i + data2_i;
         sub_result = data1_i - data2_i;
         and_result = data1_i & data2_i;
         or_result  = data1_i | data2_i;
+        xor_result = data1_i ^ data2_i;
+        sll_result = data1_i << data2_i[4:0];
+        srl_result = data1_i >> data2_i[4:0];
+        sra_result = data1_i >>> data2_i[4:0];
     end
 
     always @(*) begin
@@ -60,7 +90,7 @@ module alu (
             end
             `SUB: begin
                 data_o = sub_result;
-                zero_o = (sub_result == 32'd0) ? 1'b1 : 1'b0;
+                zero_o = 1'b0;
             end
             `AND: begin
                 data_o = and_result;
@@ -74,9 +104,25 @@ module alu (
                 data_o = {31'd0, sub_result[31]};
                 zero_o = 1'b0;
             end
-            default: begin
-                data_o = 32'd0;
+            `XOR: begin
+                data_o = xor_result;
+                zero_o = (xor_result == 32'd0) ? 1'b1 : 1'b0; //For BEQ
+            end
+            `SLL: begin
+                data_o = sll_result;
                 zero_o = 1'b0;
+            end
+            `SRL: begin
+                data_o = srl_result;
+                zero_o = 1'b0;
+            end
+            `SRA: begin
+                data_o = sra_result;
+                zero_o = 1'b0;
+            end
+            default: begin //BNE
+                data_o = 32'd0;
+                zero_o = (xor_result == 32'd0) ? 1'b0 : 1'b1; //For BNE
             end
         endcase
     end
--- control.v
+++ control.v
@@ -21,22 +21,37 @@ module control (
     output       MemtoReg_o, MemWrite_o, MemRead_o;
     output       ALUSrc_o, RegWrite_o;
     output [1:0] ALUOp_o;
+
     //ISA to be handled
     //ADD:  0000000      rs2 rs1 000 rd          0110011
+    //ADDI: IMM[11:0]        rs1 000 rd          0010011
     //LW:   IMM[11:0]        rs1 010 rd          0000011
     //SW:   IMM[11:5]    rs2 rs1 010 IMM[4:0]    0100011
     //JALR: IMM[11:0]        rs1 000 rd          1100111 //Byte address
 
     //SUB:  0100000      rs2 rs1 000 rd          0110011
-    //BEQ:  IMM[12,10:5] rs2 rs1 000 IMM[4:1,11] 1100011 //Halfword address //Partially handled by ALU
 
     //AND:  0000000      rs2 rs1 111 rd          0110011
+    //ANDI: IMM[11:0]        rs1 111 rd          0010011
 
     //OR:   0000000      rs2 rs1 110 rd          0110011
+    //ORI:  IMM[11:0]        rs1 110 rd          0010011
 
-    //SLT:  0000000      rs2 rs1 010 rd          0110011
+    //XOR:  0000000      rs2 rs1 100 rd          0110011
+    //XORI: IMM[11:0]        rs1 100 rd          0010011
+    //BEQ:  IMM[12,10:5] rs2 rs1 000 IMM[4:1,11] 1100011 //Halfword address //Partially handled by ALU, PC + imm is outside of ALU
+    //BNE:  IMM[12,10:5] rs2 rs1 001 IMM[4:1,11] 1100011 //Halfword address //Partially handled by ALU, PC + imm is outside of ALU
     
-    //JAL:  IMM[20,10:1,11,19:12]    rd          1101111 //Halfword address //Not handled by ALU
+    //SLLI: 0000000      sha rs1 001 rd          0010011
+
+    //SRLI: 0000000      sha rs1 101 rd          0010011
+    
+    //SRAI: 0100000      sha rs1 101 rd          0010011
+    
+    //SLT:  0000000      rs2 rs1 010 rd          0110011
+    //SLTI: IMM[11:0]        rs1 010 rd          0010011
+    
+    //JAL:  IMM[20,10:1,11,19:12]    rd          1101111 //Halfword address //Not handled by ALU, PC + imm is outside of ALU
 
     //If operation is JALR, JALR = 1
     //                else, JALR = 0
@@ -46,7 +61,7 @@ module control (
     //                else, JAL = 0
     assign Jal_o = (Opcode_i == 7'b1101111) ? 1 : 0;
 
-    //If operation is BEQ, Branch = 1
+    //If operation is BEQ or BNE, Branch = 1
     //                else, Branch = 0
     assign Branch_o = (Opcode_i == 7'b1100011) ? 1 : 0;
 
@@ -60,17 +75,21 @@ module control (
 
     //If operation is LW, MemRead = 1
     //                else, MemRead = 0
-    assign MemRead_o = (Opcode_i == 7'0000011) ? 1 : 0;
+    assign MemRead_o = (Opcode_i == 7'b0000011) ? 1 : 0;
 
-    //If operation is ADD or LW or JALR or SUB or AND or OR or SLT or JAL, RegWrite = 1
-    //                SW or BEQ, RegWrite = 0
+    //If operation is ADD or ADDI or LW or JALR or SUB or AND or ANDI or OR or ORI or SLT or SLTI or JAL or XOR or XORI or SLLI or SRLI or SRAI, RegWrite = 1
+    //                SW or BEQ or BNE, RegWrite = 0
     assign RegWrite_o = ((Opcode_i == 7'b0100011) | (Opcode_i == 7'b1100011)) ? 0 : 1;
 
-    //If operation is ADD or SUB or BEQ or AND or OR or SLT, ALUSrc = 0
-    //                LW or SW or JALR, ALUSrc = 1
+    //If operation is ADD or SUB or BEQ or BNE or AND or OR or XOR or SLT, ALUSrc = 0
+    //                LW or SW or JALR or ADDI or ANDI or ORI or XORI or SLLI or SRLI or SRAI or SLTI, ALUSrc = 1
     assign ALUSrc_o = ((Opcode_i == 7'b0110011) | (Opcode_i == 7'b1100011)) ? 0 : 1;
 
-    //ALUOp are two bits. 2'd0 represents 0110011, 2'd1 represents 1100011, 2'd2 represents others.
-    assign ALUOp_o = (Opcode_i == 7'b0110011) ? 2'd0 :
-                     (Opcode_i == 7'b1100011) ? 2'd1 : 2'd2;
+    //ALUOp are two bits. //2'd0 represents 0110011 (R), 2'd1 represents 0010011 (I), 2'd2 represents 1100011 (B), 2'd3 represents others
+    assign ALUOp_o = ((Opcode_i[5] == 1'b1) && (Opcode_i[4] == 1'b1)) ? 2'd0 :
+                     ((Opcode_i[5] == 1'b0) && (Opcode_i[4] == 1'b1)) ? 2'd1 :
+                     ((Opcode_i[6] == 1'b1) && (Opcode_i[2] == 1'b0)) ? 2'd2 : 2'd3;
+    // assign ALUOp_o = (Opcode_i == 7'b0110011) ? 2'd0 :
+    //                  (Opcode_i == 7'b0010011) ? 2'd1 :
+    //                  (Opcode_i == 7'b1100011) ? 2'd2 : 2'd3;
 endmodule
